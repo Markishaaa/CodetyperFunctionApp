7e219952ad9878a84120e2e02d9d1f1c518dda14
marko <marko.tanasic01@gmail.com>
2024-10-14 15:17:21 +0200

    language service class, component

--- codetyper.client/src/services/LanguageService.ts
+++ codetyper.client/src/services/LanguageService.ts
@@ -1,9 +1,16 @@
 import { API_BASE_URL } from "../configurations/config";
+import Language from "../models/Language";
 import { ApiResponse, fetchWithToken } from "./AuthService";
+
+interface ApiGetResponse {
+    success: boolean;
+    message?: string;
+    data: Language[] | null;
+}
 
 export const addLanguage = async (name: string): Promise<ApiResponse> => {
     try {
-        const response = await fetchWithToken(`${API_BASE_URL}/api/addLanguage`, {
+        const response = await fetchWithToken(`/api/AddLanguage`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -26,3 +33,28 @@ export const addLanguage = async (name: string): Promise<ApiResponse> => {
         }
     }
 };
+
+export const getAllLanguages = async (): Promise<ApiGetResponse> => {
+    try {
+        const response = await fetch(`${API_BASE_URL}/api/languages/GetAll`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        if (!response.ok) {
+            const errorMessage = await response.text();
+            throw new Error(errorMessage);
+        }
+
+        const data: Language[] = await response.json();
+        return { success: true, data };
+    } catch (error) {
+        if (error instanceof Error) {
+            return { success: false, message: error.message, data: null };
+        } else {
+            return { success: false, message: "An unknown error occurred.", data: null };
+        }
+    }
+}
