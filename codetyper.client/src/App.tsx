a5d27f93ac3d938a85517596787f5d6ae97d4fe0
marko <marko.tanasic01@gmail.com>
2024-10-14 16:37:07 +0200

    add typingGame reset trigger

--- codetyper.client/src/App.tsx
+++ codetyper.client/src/App.tsx
@@ -1,25 +1,49 @@
-import { Outlet } from 'react-router';
+import { Outlet, useLocation, useNavigate } from 'react-router';
 import './styles/App.css';
 import NavbarComponent from './components/navbar/NavbarComponent';
 import { logoutUser } from './services/AuthService';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import TypingGame from './components/typingGame/TypingGame';
+import TaskList from './containers/task/taskList/TaskList';
+import { useState } from 'react';
 
 function App() {
+    const location = useLocation();
+    const isFrontPage = location.pathname === "/";
+
+    const [resetTrigger, setResetTrigger] = useState<number>(0);
+    const navigate = useNavigate();
+
+    const handleResetAndNavigate = () => {
+        setResetTrigger(prev => prev + 1);
+        navigate('/');
+    };
+
     return (
         <div className="App">
-            <div className="content">
-                <NavbarComponent onLogout={logoutUser} />
-                <Outlet />
-                <ToastContainer
-                    position="bottom-center"
-                    autoClose={5000}
-                    newestOnTop={true}
-                    closeOnClick
-                    rtl={false}
-                    pauseOnHover
-                />
+            <div className="content flex-column">
+                <NavbarComponent onLogout={logoutUser} onReset={handleResetAndNavigate} />
+                <div className="padding">
+                    {isFrontPage ? (<>
+                        <TypingGame resetTrigger={resetTrigger}></TypingGame>
+                        <hr className="hr"></hr>
+                        <TaskList></TaskList>
+                    </>) : (<>
+                        <Outlet />
+                     </>)}
+                </div>
             </div>
+            <ToastContainer
+                className="toast"
+                position="bottom-center"
+                autoClose={5000}
+                newestOnTop={true}
+                closeOnClick
+                rtl={false}
+                pauseOnHover
+                closeButton={false}
+            />
         </div>
     );
 }
--- codetyper.client/src/main.tsx
+++ codetyper.client/src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './styles/index.css'
+// import './styles/index.css'
+import 'chota';
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/Routes.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
+    //<React.StrictMode>
         <RouterProvider router={router} />
-    </React.StrictMode>
+    //</React.StrictMode>
 )
