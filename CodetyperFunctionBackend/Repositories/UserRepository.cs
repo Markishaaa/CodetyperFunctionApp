a7ffbfebaec22a0d02e99dbead643f3dfe040572
marko <marko.tanasic01@gmail.com>
2024-10-15 22:44:56 +0200

    added user repository class

---
+++ CodetyperFunctionBackend/Repositories/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using CodetyperFunctionBackend.Model;
+using CodetyperFunctionBackend.Services;
+using Dapper;
+
+namespace CodetyperFunctionBackend.Repositories
+{
+    internal class UserRepository
+    {
+        private readonly DatabaseService _dbService;
+
+        public UserRepository(DatabaseService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        public async Task<User?> GetUserByIdAsync(string creatorId)
+        {
+            string query = $@"
+                SELECT {User.Fields.UserId}, {User.Fields.Username}, {User.Fields.RoleName}
+                FROM {User.Fields.TableName}
+                WHERE UserId = @CreatorId";
+
+            return await _dbService.ExecuteAsync(async conn =>
+            {
+                return await conn.QueryFirstOrDefaultAsync<User>(query, new { CreatorId = creatorId });
+            });
+        }
+    }
+}
