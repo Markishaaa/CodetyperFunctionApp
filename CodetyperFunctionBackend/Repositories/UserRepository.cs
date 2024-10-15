804ed742bafff44d8b676fbb667b92e60569838f
marko <marko.tanasic01@gmail.com>
2024-10-16 00:33:20 +0200

    added/updated user service and repo

--- CodetyperFunctionBackend/Repositories/UserRepository.cs
+++ CodetyperFunctionBackend/Repositories/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using CodetyperFunctionBackend.Model;
+﻿using CodetyperFunctionBackend.DTOs;
+using CodetyperFunctionBackend.Model;
 using CodetyperFunctionBackend.Services;
 using Dapper;
 
@@ -24,6 +25,59 @@ namespace CodetyperFunctionBackend.Repositories
             {
                 return await conn.QueryFirstOrDefaultAsync<User>(query, new { CreatorId = creatorId });
             });
+        }
+
+        public async Task<bool> IsUsernameTakenAsync(string username)
+        {
+            string checkUserQuery = $"SELECT COUNT(1) " +
+                $"FROM {User.Fields.TableName} " +
+                $"WHERE {User.Fields.Username} = @Username";
+
+            return await _dbService.ExecuteAsync(async conn =>
+            {
+                var result = await conn.ExecuteScalarAsync<int>(checkUserQuery, new { Username = username });
+                return result > 0;
+            });
+        }
+
+        public async Task AddUserAsync(User user)
+        {
+            string insertUserQuery = $"INSERT INTO {User.Fields.TableName} ({User.Fields.UserId}, {User.Fields.Username}, " +
+                $"{User.Fields.PasswordHash}, {User.Fields.Email}, {User.Fields.RoleName}, " +
+                $"{User.Fields.CreatedAt}, {User.Fields.UpdatedAt}) " +
+                "VALUES (@UserId, @Username, @PasswordHash, @Email, @RoleName, @CreatedAt, @UpdatedAt)";
+
+            await _dbService.ExecuteAsync(async conn =>
+            {
+                await conn.ExecuteAsync(insertUserQuery, user);
+            });
+        }
+
+        public async Task<UserDto?> GetUserByUsernameAsync(string username)
+        {
+            var query = @$"
+                SELECT {User.Fields.PasswordHash}, {User.Fields.RoleName}, {User.Fields.UserId}
+                FROM {User.Fields.TableName}  
+                WHERE {User.Fields.Username} = @Username";
+
+            var result = await _dbService.ExecuteAsync(async conn =>
+            {
+                var result = await conn.QuerySingleOrDefaultAsync(query, new { Username = username });
+                return result;
+            });
+
+            if (result != null)
+            {
+                return new UserDto
+                {
+                    UserId = result.UserId,
+                    Username = result.Username,
+                    PasswordHash = result.PasswordHash,
+                    RoleName = result.RoleName
+                };
+            }
+
+            return null;
         }
     }
 }
