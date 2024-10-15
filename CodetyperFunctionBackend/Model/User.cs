486bb04975451572c168521e3a7ea7d599d2b033
marko <marko.tanasic01@gmail.com>
2024-10-15 18:56:10 +0200

    added user and task database fields constants

--- CodetyperFunctionBackend/Model/User.cs
+++ CodetyperFunctionBackend/Model/User.cs
@@ -18,5 +18,17 @@ namespace CodetyperFunctionBackend.Model
         public DateTime UpdatedAt { get; set; }
         [JsonPropertyName("roleName")]
         public string? RoleName { get; set; }
+
+        public static class Fields
+        {
+            public const string TableName = "Users";
+            public const string UserId = "UserId";
+            public const string Username = "Username";
+            public const string PasswordHash = "PasswordHash";
+            public const string Email = "Email";
+            public const string CreatedAt = "CreatedAt";
+            public const string UpdatedAt = "UpdatedAt";
+            public const string RoleName = "RoleName";
+        }
     }
 }
